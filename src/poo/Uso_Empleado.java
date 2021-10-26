d4ac269f2797ada4e05e42c86d273daac4dfefdf
SamuelArnaiz <samueldelarosacr@gmail.com>
2021-10-27 00:06:06 +0200

    Sobrecarga de Constructores

--- src/poo/Uso_Empleado.java
+++ src/poo/Uso_Empleado.java
@@ -21,11 +21,12 @@ public class Uso_Empleado {
 		System.out.println("Nombre: " + empleado3.dame_nombre()+ " Sueldo: " + empleado3.dame_sueldo()
 		+" Fecha de Alta: " + empleado3.dime_fecha());*/
 		
-		Empleado[] misEmpleados=new Empleado[3];
+		Empleado[] misEmpleados=new Empleado[4];
 		
 		misEmpleados[0]=new Empleado("Paco Gómez", 85000, 1990, 12, 17);
 		misEmpleados[1]=new Empleado("Ana López", 95000 , 1995, 12, 17);
 		misEmpleados[2]=new Empleado("María Martín", 105000, 2002, 03 , 15);
+		misEmpleados[3]=new Empleado("Antonio Fernandez");
 		
 		/*for(int i=0;i<3;i++) {
 			misEmpleados[i].sube_sueldo(5);
@@ -57,6 +58,12 @@ class Empleado{
 		sueldo=sue;
 		GregorianCalendar calendario=new GregorianCalendar(agno, (mes-1) , dia);
 		altaContrato=calendario.getTime();
+	}
+	
+	public Empleado(String nom) {
+		
+		this(nom,30000, 2000,01,01);
+		
 	}
 	
 	public String dame_nombre() { //getter
